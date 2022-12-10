218558b8ce1c1bc2137de7f014e00cecf7635c43
Bsaaristo <92605542+Bsaaristo@users.noreply.github.com>
2022-12-09 18:25:59 -0600

    Add files via upload

---
+++ Saaristo-Asg8-Sales/Form1.Designer.cs
@@ -0,0 +1,344 @@
+﻿namespace Saaristo_Asg8_Sales
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBoxTotAndComisssions = new System.Windows.Forms.GroupBox();
+            this.groupBoxSalesData = new System.Windows.Forms.GroupBox();
+            this.listBoxSalesDataInp = new System.Windows.Forms.ListBox();
+            this.textBoxSV = new System.Windows.Forms.TextBox();
+            this.textBoxLV = new System.Windows.Forms.TextBox();
+            this.textBoxAvg = new System.Windows.Forms.TextBox();
+            this.textBoxTotal = new System.Windows.Forms.TextBox();
+            this.textBoxSVC = new System.Windows.Forms.TextBox();
+            this.textBoxLVC = new System.Windows.Forms.TextBox();
+            this.textBoxAvgC = new System.Windows.Forms.TextBox();
+            this.textBoxTotalC = new System.Windows.Forms.TextBox();
+            this.textBoxSVNS = new System.Windows.Forms.TextBox();
+            this.textBoxLVNS = new System.Windows.Forms.TextBox();
+            this.textBoxAvgNS = new System.Windows.Forms.TextBox();
+            this.textBoxTotalNS = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.numericUpDownComishPercent = new System.Windows.Forms.NumericUpDown();
+            this.groupBoxTotAndComisssions.SuspendLayout();
+            this.groupBoxSalesData.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownComishPercent)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBoxTotAndComisssions
+            // 
+            this.groupBoxTotAndComisssions.Controls.Add(this.numericUpDownComishPercent);
+            this.groupBoxTotAndComisssions.Controls.Add(this.buttonExit);
+            this.groupBoxTotAndComisssions.Controls.Add(this.buttonCalculate);
+            this.groupBoxTotAndComisssions.Controls.Add(this.label7);
+            this.groupBoxTotAndComisssions.Controls.Add(this.label6);
+            this.groupBoxTotAndComisssions.Controls.Add(this.label5);
+            this.groupBoxTotAndComisssions.Controls.Add(this.label4);
+            this.groupBoxTotAndComisssions.Controls.Add(this.label3);
+            this.groupBoxTotAndComisssions.Controls.Add(this.label2);
+            this.groupBoxTotAndComisssions.Controls.Add(this.label1);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxTotalNS);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxAvgNS);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxLVNS);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxSVNS);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxTotalC);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxAvgC);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxLVC);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxSVC);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxTotal);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxAvg);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxLV);
+            this.groupBoxTotAndComisssions.Controls.Add(this.textBoxSV);
+            this.groupBoxTotAndComisssions.Location = new System.Drawing.Point(306, 35);
+            this.groupBoxTotAndComisssions.Name = "groupBoxTotAndComisssions";
+            this.groupBoxTotAndComisssions.Size = new System.Drawing.Size(466, 375);
+            this.groupBoxTotAndComisssions.TabIndex = 0;
+            this.groupBoxTotAndComisssions.TabStop = false;
+            this.groupBoxTotAndComisssions.Text = "Totals And Comissions";
+            // 
+            // groupBoxSalesData
+            // 
+            this.groupBoxSalesData.Controls.Add(this.listBoxSalesDataInp);
+            this.groupBoxSalesData.Location = new System.Drawing.Point(2, 35);
+            this.groupBoxSalesData.Name = "groupBoxSalesData";
+            this.groupBoxSalesData.Size = new System.Drawing.Size(287, 375);
+            this.groupBoxSalesData.TabIndex = 1;
+            this.groupBoxSalesData.TabStop = false;
+            this.groupBoxSalesData.Text = "Sales Data: 25 Records";
+            // 
+            // listBoxSalesDataInp
+            // 
+            this.listBoxSalesDataInp.FormattingEnabled = true;
+            this.listBoxSalesDataInp.ItemHeight = 16;
+            this.listBoxSalesDataInp.Location = new System.Drawing.Point(20, 32);
+            this.listBoxSalesDataInp.Name = "listBoxSalesDataInp";
+            this.listBoxSalesDataInp.Size = new System.Drawing.Size(254, 324);
+            this.listBoxSalesDataInp.TabIndex = 0;
+            // 
+            // textBoxSV
+            // 
+            this.textBoxSV.Location = new System.Drawing.Point(147, 65);
+            this.textBoxSV.Name = "textBoxSV";
+            this.textBoxSV.ReadOnly = true;
+            this.textBoxSV.Size = new System.Drawing.Size(97, 22);
+            this.textBoxSV.TabIndex = 0;
+            // 
+            // textBoxLV
+            // 
+            this.textBoxLV.Location = new System.Drawing.Point(147, 93);
+            this.textBoxLV.Name = "textBoxLV";
+            this.textBoxLV.ReadOnly = true;
+            this.textBoxLV.Size = new System.Drawing.Size(97, 22);
+            this.textBoxLV.TabIndex = 1;
+            // 
+            // textBoxAvg
+            // 
+            this.textBoxAvg.Location = new System.Drawing.Point(147, 121);
+            this.textBoxAvg.Name = "textBoxAvg";
+            this.textBoxAvg.ReadOnly = true;
+            this.textBoxAvg.Size = new System.Drawing.Size(97, 22);
+            this.textBoxAvg.TabIndex = 2;
+            // 
+            // textBoxTotal
+            // 
+            this.textBoxTotal.Location = new System.Drawing.Point(147, 149);
+            this.textBoxTotal.Name = "textBoxTotal";
+            this.textBoxTotal.ReadOnly = true;
+            this.textBoxTotal.Size = new System.Drawing.Size(97, 22);
+            this.textBoxTotal.TabIndex = 3;
+            // 
+            // textBoxSVC
+            // 
+            this.textBoxSVC.Location = new System.Drawing.Point(257, 65);
+            this.textBoxSVC.Name = "textBoxSVC";
+            this.textBoxSVC.ReadOnly = true;
+            this.textBoxSVC.Size = new System.Drawing.Size(97, 22);
+            this.textBoxSVC.TabIndex = 4;
+            // 
+            // textBoxLVC
+            // 
+            this.textBoxLVC.Location = new System.Drawing.Point(257, 93);
+            this.textBoxLVC.Name = "textBoxLVC";
+            this.textBoxLVC.ReadOnly = true;
+            this.textBoxLVC.Size = new System.Drawing.Size(97, 22);
+            this.textBoxLVC.TabIndex = 5;
+            // 
+            // textBoxAvgC
+            // 
+            this.textBoxAvgC.Location = new System.Drawing.Point(257, 121);
+            this.textBoxAvgC.Name = "textBoxAvgC";
+            this.textBoxAvgC.ReadOnly = true;
+            this.textBoxAvgC.Size = new System.Drawing.Size(97, 22);
+            this.textBoxAvgC.TabIndex = 6;
+            // 
+            // textBoxTotalC
+            // 
+            this.textBoxTotalC.Location = new System.Drawing.Point(257, 149);
+            this.textBoxTotalC.Name = "textBoxTotalC";
+            this.textBoxTotalC.ReadOnly = true;
+            this.textBoxTotalC.Size = new System.Drawing.Size(97, 22);
+            this.textBoxTotalC.TabIndex = 7;
+            // 
+            // textBoxSVNS
+            // 
+            this.textBoxSVNS.Location = new System.Drawing.Point(360, 65);
+            this.textBoxSVNS.Name = "textBoxSVNS";
+            this.textBoxSVNS.ReadOnly = true;
+            this.textBoxSVNS.Size = new System.Drawing.Size(97, 22);
+            this.textBoxSVNS.TabIndex = 8;
+            // 
+            // textBoxLVNS
+            // 
+            this.textBoxLVNS.Location = new System.Drawing.Point(360, 93);
+            this.textBoxLVNS.Name = "textBoxLVNS";
+            this.textBoxLVNS.ReadOnly = true;
+            this.textBoxLVNS.Size = new System.Drawing.Size(97, 22);
+            this.textBoxLVNS.TabIndex = 9;
+            // 
+            // textBoxAvgNS
+            // 
+            this.textBoxAvgNS.Location = new System.Drawing.Point(360, 121);
+            this.textBoxAvgNS.Name = "textBoxAvgNS";
+            this.textBoxAvgNS.ReadOnly = true;
+            this.textBoxAvgNS.Size = new System.Drawing.Size(97, 22);
+            this.textBoxAvgNS.TabIndex = 10;
+            // 
+            // textBoxTotalNS
+            // 
+            this.textBoxTotalNS.Location = new System.Drawing.Point(360, 149);
+            this.textBoxTotalNS.Name = "textBoxTotalNS";
+            this.textBoxTotalNS.ReadOnly = true;
+            this.textBoxTotalNS.Size = new System.Drawing.Size(97, 22);
+            this.textBoxTotalNS.TabIndex = 11;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(52, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(88, 16);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "Comission %:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(40, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 16);
+            this.label2.TabIndex = 13;
+            this.label2.Text = "Smallest Value:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(47, 96);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(93, 16);
+            this.label3.TabIndex = 14;
+            this.label3.Text = "Largest Value:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(106, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(34, 16);
+            this.label4.TabIndex = 15;
+            this.label4.Text = "Avg:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(99, 155);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(41, 16);
+            this.label5.TabIndex = 16;
+            this.label5.Text = "Total:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(264, 36);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(80, 16);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "Comissions:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(372, 36);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(62, 16);
+            this.label7.TabIndex = 18;
+            this.label7.Text = "Net Sale:";
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(86, 229);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(116, 32);
+            this.buttonCalculate.TabIndex = 19;
+            this.buttonCalculate.Text = "Calculate";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(238, 229);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(116, 32);
+            this.buttonExit.TabIndex = 20;
+            this.buttonExit.Text = "Exit";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // numericUpDownComishPercent
+            // 
+            this.numericUpDownComishPercent.Location = new System.Drawing.Point(146, 30);
+            this.numericUpDownComishPercent.Name = "numericUpDownComishPercent";
+            this.numericUpDownComishPercent.Size = new System.Drawing.Size(91, 22);
+            this.numericUpDownComishPercent.TabIndex = 21;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.groupBoxSalesData);
+            this.Controls.Add(this.groupBoxTotAndComisssions);
+            this.Name = "Form1";
+            this.Text = "Saaristo Asg8 Sales";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBoxTotAndComisssions.ResumeLayout(false);
+            this.groupBoxTotAndComisssions.PerformLayout();
+            this.groupBoxSalesData.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDownComishPercent)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBoxTotAndComisssions;
+        private System.Windows.Forms.NumericUpDown numericUpDownComishPercent;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxTotalNS;
+        private System.Windows.Forms.TextBox textBoxAvgNS;
+        private System.Windows.Forms.TextBox textBoxLVNS;
+        private System.Windows.Forms.TextBox textBoxSVNS;
+        private System.Windows.Forms.TextBox textBoxTotalC;
+        private System.Windows.Forms.TextBox textBoxAvgC;
+        private System.Windows.Forms.TextBox textBoxLVC;
+        private System.Windows.Forms.TextBox textBoxSVC;
+        private System.Windows.Forms.TextBox textBoxTotal;
+        private System.Windows.Forms.TextBox textBoxAvg;
+        private System.Windows.Forms.TextBox textBoxLV;
+        private System.Windows.Forms.TextBox textBoxSV;
+        private System.Windows.Forms.GroupBox groupBoxSalesData;
+        private System.Windows.Forms.ListBox listBoxSalesDataInp;
+    }
+}
+
---
+++ Saaristo-Asg8-Sales/Form1.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.NetworkInformation;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Saaristo_Asg8_Sales
+{
+    public partial class Form1 : Form
+    {
+        const int SALES_COUNT = 25;
+        double[] arraySalesData = new double[SALES_COUNT];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+
+            clearSalesDataLabels();
+
+            // Get the sales values.
+            double smallestSale = getSmallestSale();
+            double largestSale = getLargestSale();
+            double totalSales = getTotalSales();
+            double averageSale = totalSales / SALES_COUNT; 
+
+
+            // Display the sales data
+            textBoxSV.Text = smallestSale.ToString("c");
+            textBoxLV.Text = largestSale.ToString("c");
+            textBoxAvg.Text = averageSale.ToString("c");
+            textBoxTotal.Text = totalSales.ToString("c");
+
+            // Get the commission data.
+            double commissionRate = (double)numericUpDownComishPercent.Value;
+
+
+            // Calculate and display SMALLEST commission and net sale.
+            double smallestCommission = calculateCommission(smallestSale, commissionRate);
+            double smallestNetSale = smallestSale - smallestCommission;
+            textBoxSVC.Text = smallestCommission.ToString("c");
+            textBoxSVNS.Text = smallestNetSale.ToString("c");
+
+            // Calculate and display LARGEST commission and net sale
+            double largestCommission = calculateCommission(largestSale, commissionRate);
+            double largestNetSale = largestSale - largestCommission;
+            textBoxLVC.Text = largestCommission.ToString("c");
+            textBoxLVNS.Text = largestNetSale.ToString("c");
+
+            // Calculate and display AVERAGE commission and net sale
+            double avgCommission = (largestCommission + smallestCommission) / 2;
+            double avgNetSale = averageSale - avgCommission;
+            textBoxAvgC.Text = avgCommission.ToString("c");
+            textBoxAvgNS.Text = avgNetSale.ToString("c");
+
+            // Calculate and display TOTAL commission and net sale
+            double totalCommission = largestCommission + smallestCommission;
+            double totalNetSale = totalSales - totalCommission;
+            textBoxTotalC.Text = totalCommission.ToString("c");
+            textBoxTotalNS.Text = totalNetSale.ToString("c");
+        }
+
+       // private double calculateAverage(double a, double b)
+        //{
+          //  throw new NotImplementedException();
+            //double avg = arraySalesData.Sum() / 2;
+           // return avg;
+    //    }
+    
+
+        private double calculateCommission(double a, double b)
+        {
+            double calculatedCommission = a * (b /100);
+            return calculatedCommission;
+        }
+
+        private double getTotalSales()
+        {
+            double sum = arraySalesData.Sum();
+            return sum;
+        }
+
+        private double getLargestSale()
+        {
+            double largestSale = arraySalesData.Max();
+            return largestSale;
+        }
+
+        private double getSmallestSale()
+        {
+            double smallestSale = arraySalesData.Min();
+            return smallestSale;
+        }
+
+        private void clearSalesDataLabels()
+        {
+            textBoxAvg.Text = null;
+            textBoxAvgC.Text = null;
+            textBoxAvgNS.Text = null;
+            textBoxLV.Text = null;
+            textBoxLVC.Text = null;
+            textBoxLVNS.Text = null;
+            textBoxSV.Text = null;
+            textBoxSVC.Text = null;
+            textBoxSVNS.Text = null;
+            textBoxTotal.Text = null;
+            textBoxTotalC.Text = null;
+            textBoxTotalNS.Text = null; 
+
+        }
+
+        private void loadSalesDataIntoArray()
+        {
+            StreamReader inputReader = new StreamReader("sales.txt");
+
+            for (int i = 0; i < SALES_COUNT; i++)
+            {
+                if (!inputReader.EndOfStream)
+                    arraySalesData[i] = double.Parse(inputReader.ReadLine());
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loadSalesDataIntoArray();
+            loadArrayIntoListbox();
+
+            groupBoxSalesData.Text = "Sales Data: " + listBoxSalesDataInp.Items.Count.ToString() + " Records";
+        }
+
+        private void loadArrayIntoListbox()
+        {
+            for(int X=0; X < arraySalesData.Length; X++)
+            {
+                listBoxSalesDataInp.Items.Add(arraySalesData[X]);
+            }
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Environment.Exit(0);
+        }
+    }
+}
